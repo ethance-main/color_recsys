5e5caab3cd0db45e41ac9babed17428c60c9f68e
ethance <hance.ethan@gmail.com>
2026-04-22 10:37:35 -0400

    modified app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,31 @@ import pandas as pd
 levels = [0, 64, 128, 192, 255]
 colors = list(itertools.product(levels, levels, levels))
 
-sample = random.sample(colors, 25)
 st.title("Color Ratings")
-responses = []
 
-for i, (r, g, b) in enumerate(sample):
-    color_box = f"rgb({r},{g},{b})"
-    
-    st.markdown(
-        f"<div style='width:100px;height:100px;background-color:{color_box}'></div>",
-        unsafe_allow_html=True
-    )
-    
-    rating = st.slider(f"Color {i+1}", 1, 10)
-    
-    responses.append((r, g, b, rating))
+if "sample" not in st.session_state:
+    st.session_state.sample = random.sample(colors, 25)
+sample = st.session_state.sample
 
+with st.form("color_quiz"):
+    responses = []
+    for i, (r, g, b) in enumerate(sample):
+        color_box = f"rgb({r},{g},{b})"
+    
+        st.markdown(
+            f"<div style='width:100px;height:100px;background-color:{color_box}'></div>",
+            unsafe_allow_html=True
+        )
+    
+        rating = st.slider(f"Color {i+1}", 1, 10)
+        responses.append((r, g, b, rating))
+    submitted = st.form_submit_button("Submit Ratings")
+
+if submitted:
+    df = pd.DataFrame(responses, columns=['R','G','B','rating'])
+    df.to_csv("responses.csv", mode='a', header=False, index=False)
+    st.success("Ratings submitted successfully!")
+    st.session_state.sample = random.sample(colors, 25)
 
 df = pd.DataFrame(responses, columns=['R','G','B','rating'])
 df.to_csv("responses.csv", mode='a', header=False, index=False)
